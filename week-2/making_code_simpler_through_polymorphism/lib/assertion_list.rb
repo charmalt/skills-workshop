a76ebe874cf4daaca2a3affb313bc62878779821
Charlene <charlenechetcuti@gmail.com>
2018-04-26 10:33:00 +0100

    oop and polymorphism workshop

--- week-2/making_code_simpler_through_polymorphism/lib/assertion_list.rb
+++ week-2/making_code_simpler_through_polymorphism/lib/assertion_list.rb
@@ -9,11 +9,7 @@ class AssertionList
 
   def run_all_assertions
     @assertions.each do |assertion|
-      if assertion.is_a?(TruthyAssertion)
-        assertion.truthy
-      elsif assertion.is_a?(EqualAssertion)
-        assertion.equal
-      end
+      assertion.check
     end
   end
 end
